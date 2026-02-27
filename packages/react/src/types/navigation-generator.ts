371950f6eae1baaea7c39726005a96cfb2561402
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-02-27 16:07:32 +0330

    adding custom route type and icon type to route map.

--- packages/react/src/types/navigation-generator.ts
+++ packages/react/src/types/navigation-generator.ts
@@ -1,14 +1,19 @@
 import { ResolvedRouteItem } from "./route-item";
 
-type BuiltNavigation<AllCtx extends string> = {
+type BuiltNavigation<AllCtx extends string, Routes, Ic> = {
   getRoutes: (
     context: AllCtx,
     loggedIn: boolean,
-  ) => readonly ResolvedRouteItem[];
+  ) => readonly ResolvedRouteItem<string[], Routes, Ic>[];
 } & {
   [K in Capitalize<AllCtx> as `get${K}Routes`]: (
     loggedIn: boolean,
-  ) => readonly ResolvedRouteItem[];
+  ) => readonly ResolvedRouteItem<string[], Routes, Ic>[];
 };
 
-export type { BuiltNavigation };
+type InferRouteItem<N> =
+  N extends BuiltNavigation<infer _Ctx, infer _Routes, infer _Ic>
+    ? ReturnType<N["getRoutes"]>[number]
+    : never;
+
+export type { BuiltNavigation, InferRouteItem };
--- packages/react/src/types/route-item.ts
+++ packages/react/src/types/route-item.ts
@@ -1,31 +1,30 @@
 import { ComponentType } from "react";
 
 type Auth = "authenticated" | "guest";
-type IconType = ComponentType<{ className?: string }>;
 
-type BaseRouteItem<Ctx extends readonly string[]> = {
-  href: string;
-  Icon?: IconType;
+type BaseRouteItem<Ctx extends readonly string[], Routes, Ic> = {
+  href: Routes;
+  Icon?: Ic;
   title: string | Record<Ctx[number], string>;
   order: number | Record<Ctx[number], number>;
   auth?: Auth;
-  Children?: ChildRouteItem[] | ComponentType;
+  Children?: ChildRouteItem<Ctx, Routes, Ic>[] | ComponentType;
   showIn: Ctx;
 };
 
-type ChildRouteItem<Ctx extends readonly string[] = string[]> = Pick<
-  BaseRouteItem<Ctx>,
+type ChildRouteItem<Ctx extends readonly string[], Routes, Ic> = Pick<
+  BaseRouteItem<Ctx, Routes, Ic>,
   "href" | "auth" | "Children"
 > & {
-  title: BaseRouteItem<Ctx>["href"];
-  Icon?: IconType;
+  title: string;
+  Icon?: Ic;
 };
 
-type ResolvedRouteItem<Ctx extends readonly string[] = string[]> = {
-  href: BaseRouteItem<Ctx>["href"];
-  Icon?: IconType;
+type ResolvedRouteItem<Ctx extends readonly string[], Routes, Ic> = {
+  href: BaseRouteItem<Ctx, Routes, Ic>["href"];
+  Icon?: Ic;
   title: string;
-  Children?: ResolvedRouteItem[] | ComponentType;
+  Children?: ResolvedRouteItem<Ctx, Routes, Ic>[] | ComponentType;
 };
 
 export type { BaseRouteItem, ChildRouteItem, ResolvedRouteItem, Auth };
